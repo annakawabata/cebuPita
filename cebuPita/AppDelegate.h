c6f5e8d20a856558628bbea8fcf6bef8d3c25574
川端杏奈 <Anna2301a89>
2015-09-22 14:41:04 +0800

    Initial Commit

---
+++ cebuPita/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cebuPita
+//
+//  Created by 川端杏奈 on 2015/09/22.
+//  Copyright (c) 2015年 Anna Kawabata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cebuPita/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cebuPita
+//
+//  Created by 川端杏奈 on 2015/09/22.
+//  Copyright (c) 2015年 Anna Kawabata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
